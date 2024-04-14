714dab4a3a89449487cebeafb9922801ddc3a7dc
Aajesh <ajeshchoudhary@gmail.com>
2024-04-14 16:30:07 +0530

    find the majority of element ihn the array

--- src/com/aajesh/array/MajorityElement.java
+++ src/com/aajesh/array/MajorityElement.java
@@ -1,16 +1,20 @@
-
-public class MajorityElement {
-/* 
+package com.aajesh.array;
+/*
+ *Given an array you need to find the majority of element ihn the array
+ *
+ * Majority element is defined as the element which is appearing more than n/2
+ *time in array of length n
+ *
+ * note : if there is no majority element return -1
+ *
  * T c = o(n)
  * t s= o(1)
- * 
+ *
  */
+public class MajorityElement {
     public static void main(String[] args) {
-        
         int[] arr ={5,7,2,7,4,2,5,2,4,2,4,4,2,2,2,2,2,2,2,2};
-
         System.out.println(findMajorityOfElement(arr, arr.length));
-        
     }
 
     private static int findMajorityOfElement(int[] arr, int n) {
@@ -22,31 +26,23 @@ public class MajorityElement {
                 count++;
             }else{
                 count--;
-
             }
-
             if(count==0){
                  candidate = arr[i];
                  count=1;
             }
         }
-
         //verify if candidate is your ans or not
-
         count =0;
         for (int val : arr) {
             if (val==candidate) {
                 count++;
             }
         }
-
-        if(count>n/2){  
+        if(count > n/2){
             return candidate;
-
         }else{
             return -1;
         }
-
-    }
-    
-}
+    }//end of method
+}//end of class
