2baca2457425d2452f1808058cdad912416e24a9
Aajesh <ajeshchoudhary@gmail.com>
2024-04-14 15:58:31 +0530

    Smallest Number Whose Product of Digit is N

--- src/com/aajesh/array/PositiveNumber.java
+++ src/com/aajesh/array/PositiveNumber.java
@@ -1,5 +1,17 @@
+package com.aajesh.array;
 import java.util.Scanner;
 
+/*
+* Smallest Number Whose Product of Digit is N
+* Given the positive number N. you need to find the smallest number S such that Product
+* of digit of  is equal to N
+*
+* Sample Input  N =1000
+* out : 5558
+*
+* TC=o(long n)
+* Sp=0(0)
+* */
 public class PositiveNumber{
 
     public static void main(String[] args) {
@@ -9,20 +21,17 @@ public class PositiveNumber{
     }
 
     private static  int solution(int input) {
-        
         String ans="";
-
-       for (int div = 9; div>=2; div--) {
+        for (int div = 9; div>=2; div--) {
             while(input%div ==0){
-                input = input/div;
-                ans=div+ans;
+                input = input / div;
+                ans = div + ans;
             }
-       }
-
-       if(input!=1){
-        return -1;
-       }else{
-        return Integer.parseInt(ans);
-       }
-    }
+        }
+        if(input != 1){
+            return -1;
+        }else{
+            return Integer.parseInt(ans);
+        }
+    }//end method
 }
